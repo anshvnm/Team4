61ca9627b0233f59e45ef96d7a9741980f67f45f
Peter H <phwang25@terpmail.umd.edu>
2023-01-10 16:03:18 -0500

    Implemented and tested PacMan.get_valid_moves()

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -18,20 +18,21 @@ public class PacMan {
     // Create the resulting ArrayList
     ArrayList<Location> result = new ArrayList<>();
     // Checks the PacMan surroundings for a wall and adds to the "valid" list of moves accordingly
+    // Check if PacMan is out of bounds  
     // Right
-    if (!(myMap.getLoc(myLoc.shift(1, 0)).contains(Map.Type.WALL))) {
+    if (!(myMap.getLoc(myLoc.shift(1, 0)).contains(Map.Type.WALL)) && (myLoc.x > 0 && myLoc.x <24) && (myLoc.y > 0 && myLoc.y <24)) {
       result.add(myLoc.shift(1, 0));
     }
     // Left
-    if (!(myMap.getLoc(myLoc.shift(-1, 0)).contains(Map.Type.WALL))) {
+    if (!(myMap.getLoc(myLoc.shift(-1, 0)).contains(Map.Type.WALL)) && (myLoc.x > 0 && myLoc.x <24) && (myLoc.y > 0 && myLoc.y <24)) {
       result.add(myLoc.shift(-1, 0));
     }
     // Up
-    if (!(myMap.getLoc(myLoc.shift(0, -1)).contains(Map.Type.WALL))) {
+    if (!(myMap.getLoc(myLoc.shift(0, -1)).contains(Map.Type.WALL)) && (myLoc.x > 0 && myLoc.x <24) && (myLoc.y > 0 && myLoc.y <24)) {
       result.add(myLoc.shift(0, -1));
     }
     // Down
-    if (!(myMap.getLoc(myLoc.shift(0, 1)).contains(Map.Type.WALL))) {
+    if (!(myMap.getLoc(myLoc.shift(0, 1)).contains(Map.Type.WALL)) && (myLoc.x > 0 && myLoc.x <24) && (myLoc.y > 0 && myLoc.y <24)) {
       result.add(myLoc.shift(0, 1));
     }
     return result;
