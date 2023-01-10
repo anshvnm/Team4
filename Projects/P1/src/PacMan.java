92b8ea7e06705af03833ed1161d0223428902a86
Peter H <phwang25@terpmail.umd.edu>
2023-01-10 05:03:41 -0500

    Untested version of PacMan.get_valid_moves()

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -14,7 +14,27 @@ public class PacMan {
   }
 
   public ArrayList<Location> get_valid_moves() {
-    return null;
+    // Returns all the valid moves that PacMan can make given his current position
+    // Create the resulting ArrayList
+    ArrayList<Location> result = new ArrayList<>();
+    // Checks the PacMan surroundings for a wall and adds to the "valid" list of moves accordingly
+    // Right
+    if (!(myMap.getLoc(myLoc.shift(1, 0)).contains(Map.Type.WALL))) {
+      result.add(myLoc.shift(1, 0));
+    }
+    // Left
+    if (!(myMap.getLoc(myLoc.shift(-1, 0)).contains(Map.Type.WALL))) {
+      result.add(myLoc.shift(-1, 0));
+    }
+    // Up
+    if (!(myMap.getLoc(myLoc.shift(0, -1)).contains(Map.Type.WALL))) {
+      result.add(myLoc.shift(0, -1));
+    }
+    // Down
+    if (!(myMap.getLoc(myLoc.shift(0, 1)).contains(Map.Type.WALL))) {
+      result.add(myLoc.shift(0, 1));
+    }
+    return result;
   }
 
   public boolean move() {
