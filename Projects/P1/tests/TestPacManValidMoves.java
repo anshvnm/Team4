d81225d15f30436ebd1843d3e3fa95ca93614cec
Peter H <phwang25@terpmail.umd.edu>
2023-01-10 05:30:47 -0500

    Bugged Tests for Pacman.get_valid_moves()

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -1,9 +1,28 @@
 import java.io.*;
 import junit.framework.*;
+import java.util.*;
 
 public class TestPacManValidMoves extends TestCase {
 
   public void testPacManValidMoves() throws FileNotFoundException {
-    return null;
+    NoFrame frame = new NoFrame();
+    Map map = frame.getMap();
+    HashSet<Map.Type> emptySet = new HashSet<Map.Type>();
+    emptySet.add(Map.Type.EMPTY);
+    HashSet<Map.Type> wallSet = new HashSet<Map.Type>();
+    wallSet.add(Map.Type.WALL);
+    HashSet<Map.Type> cookieSet = new HashSet<Map.Type>();
+    cookieSet.add(Map.Type.COOKIE);
+
+    // Create PacMan
+    PacMan pacman = new PacMan("pacman", new Location(9, 12), map);
+    PacManComponent pc = new PacManComponent(9, 12, 20);
+    // Create Wall
+    WallComponent wall = new WallComponent(9, 11, 20);
+
+    // Adding the PacMan to Map
+    map.add("pacman", new Location(9, 12), pc, Map.Type.PACMAN);
+    // Adding a Wall
+    map.add("wall", new Location(9, 11), wall, Map.Type.WALL);
   }
 }
