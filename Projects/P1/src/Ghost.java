a2e8ad913993ed5a86e1414471c3bd8ed4194b93
nealmachado12@gmail.com <your_email_here>
2023-01-17 23:14:26 +0100

    tried to fix the Ghost move() function

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -40,7 +40,7 @@ public class Ghost {
     if (moves.size() == 0) {
       return false;
     }
-    myMap.move(myName, moves.get(0), Map.Type.GHOST);
+    // myMap.move(myName, moves.get(0), Map.Type.GHOST);
     myLoc = moves.get(0);
     return true;
   }
